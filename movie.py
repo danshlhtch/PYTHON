48db11926d5815c35a5799a996d3c6d45987285d
Kirill <101259950+kirill-babich@users.noreply.github.com>
2023-08-03 18:08:19 +0300

    movie.py

--- movie.py
+++ movie.py
@@ -2,6 +2,7 @@ import moviepy.editor
 import os.path
 
 # Программа для конвертации видео-файлов в аудио-файлы
+print('Эта программа поможет вам преобразовать преобразовать видео-файл в аудио-файл.')
 movie = input('Введите путь в файлу:')
 file = os.path.exists(movie)
 if file == True:
