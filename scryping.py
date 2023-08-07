227e06eaa388f4803e0168112cc4d2d6fc261f09
Kirill <101259950+kirill-babich@users.noreply.github.com>
2023-08-07 12:18:19 +0300

    Add files via upload

---
+++ scryping.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+
+r = requests.get("https://re.kufar.by/l/belarus/snyat/kvartiru-dolgosrochno?cur=USD&size=30&cursor=eyJ0IjoiYWJzIiwiZiI6dHJ1ZSwicCI6MX0=")
+html = BS(r.content, 'html.parser')
+
+for el in html.select(".styles_wrapper__Q06m9"):
+    title = el.select(".styles_parameters__7zKlL")
+    titles = el.select(".styles_price__byr__lLSfd")
+    price = el.select(".styles_price__usd__HpXMa")
+    print(title[0].text)
+    print(titles[0].text)
+    print(price)
+    print("_______")
+input()
